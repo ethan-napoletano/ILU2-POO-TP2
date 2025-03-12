dd1fcdca94c5f51acadf9243c8aa55a60b550c0f
ethan-napoletano <ethan.napoletano@univ-tlse3.fr>
2025-03-12 11:43:26 +0100

    1 et 2

--- src/frontiere/BoundaryEmmenager.java
+++ src/frontiere/BoundaryEmmenager.java
@@ -27,7 +27,10 @@ public class BoundaryEmmenager {
 					break;
 
 				case 2:
-					//TODO a completer
+					System.out.println("Bienvenue villageois " + nomVisiteur);
+					int force = Clavier.entrerEntier("Quelle est votre force ?");
+					controlEmmenager.ajouterGaulois(nomVisiteur, force);
+
 					break;
 
 				default:
@@ -40,6 +43,22 @@ public class BoundaryEmmenager {
 	}
 
 	private void emmenagerDruide(String nomVisiteur) {
-		//TODO a completer
+		System.out.println("Bienvenue druide " + nomVisiteur);
+	    int forceDruide = Clavier.entrerEntier("Quelle est votre force ?");
+
+	    int effetPotionMin = 0;
+	    int effetPotionMax = 0;
+
+	    do {
+	        effetPotionMin = Clavier.entrerEntier("Quelle est la force de la potion la plus faible que vous produisez ?");
+	        effetPotionMax = Clavier.entrerEntier("Quelle est la force de la potion la plus forte que vous produisez ?");
+	        if (effetPotionMax < effetPotionMin) {
+	            System.out.println("Attention Druide, vous vous êtes trompé entre le minimum et le maximum.");
+	        }
+	    } while (effetPotionMax < effetPotionMin);
+	    
+	    
+		
+	    controlEmmenager.ajouterDruide(nomVisiteur,forceDruide,effetPotionMin, effetPotionMax);
 	}
 }
